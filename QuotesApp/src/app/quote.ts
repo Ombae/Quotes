53f5ebd8b9c85cc9baea828f42993d58e957477e
ombaejr <ombaejr@gmail.com>
2019-06-04 09:53:20 +0300

    Auto-generated commit

--- QuotesApp/src/app/quote.ts
+++
@@ -1,9 +0,0 @@
-export class Quote {
-
-    public showPublisher:boolean;
-    constructor(public quote:string, public author:string, public publisher:string,
-       public publishDate:Date, public upVote: number = 0,public downVote: number = 0){
-        this.showPublisher=false
-
-    }
-}
