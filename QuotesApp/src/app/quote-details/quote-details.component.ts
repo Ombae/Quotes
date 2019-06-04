6bff16b6e4aa4277732edd47960df15c1b7026c1
ombaejr <ombaejr@gmail.com>
2019-06-04 04:43:47 +0300

    Write functions for upvoting and downvoting a quote

--- QuotesApp/src/app/quote-details/quote-details.component.ts
+++ QuotesApp/src/app/quote-details/quote-details.component.ts
@@ -1,11 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input,EventEmitter,Output
+} from '@angular/core';
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quote:Quote;
+  @Output() isComplete = new EventEmitter<boolean>()
+  @Output() isPlus = new EventEmitter<boolean>();
+  @Output() isMinus= new EventEmitter<boolean>();
+
+
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  quotePlus(plus:boolean){
+    this.isPlus.emit(plus);
+  }
+
+  quoteMinus(minus:boolean){
+    this.isMinus.emit(minus);
+  }
 
   constructor() { }
 
